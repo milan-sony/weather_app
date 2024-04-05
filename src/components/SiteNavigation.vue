d805cf47b6b6e26113132ff367c4d1e08d034cfd
Milan Sony <milansonyofficial@gmail.com>
2024-04-05 13:06:43 +0530

    style(navbar): Navbar style changed

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,36 +1,16 @@
 <template>
     <nav class="bg-blue-700">
-        <div class="max-w-screen-lg flex flex-wrap justify-center items-center mx-auto p-4 md:max-2xl:justify-between">
+        <div class="max-w-screen-lg items-center mx-auto p-4">
             <RouterLink :to="{ name: 'Home' }">
                 <div class="flex items-center space-x-3 rtl:space-x-reverse">
                     <span
-                        class="self-center text-2xl font-extrabold uppercase tracking-wide whitespace-nowrap text-white dark:text-white max-md:pb-4"><i
+                        class="self-center text-2xl font-extrabold uppercase tracking-wider whitespace-nowrap text-white dark:text-white max-md:pb-4"><i
                             class="fa-solid fa-cloud-sun-rain"></i> Weathery
                     </span>
                 </div>
             </RouterLink>
-
-            <form class="flex items-center">
-                <label for="simple-search" class="sr-only">Search</label>
-                <div class="relative w-full">
-                    <input type="text" id="simple-search"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Search place" required />
-                </div>
-                <button type="submit"
-                    class="p-2.5 ms-2 text-sm font-medium text-white bg-blue-500 rounded-lg border border-blue-700 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-red-500 dark:focus:ring-blue-800">
-                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 20 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                    </svg>
-                    <span class="sr-only">Search</span>
-                </button>
-            </form>
-
         </div>
     </nav>
-
 </template>
 
 <script>
