acacf8036e2a820b167b088d18412acef8719300
Milan Sony <milansonyofficial@gmail.com>
2024-04-05 14:35:33 +0530

    style: date added

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -2,11 +2,21 @@
     <nav class="bg-blue-700">
         <div class="max-w-screen-lg items-center mx-auto p-4">
             <RouterLink :to="{ name: 'Home' }">
-                <div class="flex items-center space-x-3 rtl:space-x-reverse">
+                <div class="flex justify-between items-center ">
                     <span
                         class="self-center text-2xl font-extrabold uppercase tracking-wider whitespace-nowrap text-white dark:text-white max-md:pb-4"><i
                             class="fa-solid fa-cloud-sun-rain"></i> Weathery
                     </span>
+                    <div id="app" class="text-right font-bold text-sm text-white ">
+                        {{ new Date().toLocaleString(
+                            'en-us',{
+                                weekday: 'long',
+                                year: 'numeric',
+                                month: 'long',
+                                day: 'numeric'
+                            }
+                        ) }}
+                    </div>
                 </div>
             </RouterLink>
         </div>
