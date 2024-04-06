e78be2991c7501eb52e505aec47b65db01e9d71e
Milan Sony <milansonyofficial@gmail.com>
2024-04-07 00:02:09 +0530

    feat: Search location data from API displayed on main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
         <SiteNavigation />
         <!-- Search -->
         <div class="max-w-screen-sm items-center mx-auto p-5">
-            <form class="flex items-center" @submit.prevent="submitForm">
+            <form class="flex items-center" @submit.prevent="searchPlace">
                 <label for="simple-search" class="sr-only">Search</label>
                 <div class="relative w-full">
                     <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -16,7 +16,7 @@
                     </div>
                     <input type="text" id="simple-search"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 focus:outline-none block w-full ps-10 p-2.5"
-                        placeholder="Search for a city" v-model="formValue.place" @input="searchPlace" required />
+                        placeholder="Search for a city" v-model="formValue.place" required />
                 </div>
                 <button type="submit"
                     class="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300">
@@ -28,8 +28,12 @@
                     <span class="sr-only">Search</span>
                 </button>
             </form>
-            <p class="text-start text-2xl pt-4 font-bold text-white">Search : {{ formValue.place }}</p>
+            <p class="text-start text-xl pt-4 pb-2 font-bold text-white">Search result for: {{ formValue.place }}</p>
             <div class="bg-white p-4 rounded-md">
+                <div v-for="place in searchResult" :key="place.id">
+                    <button class="hover:text-blue-700 hover:font-bold font-semibold p-3">{{ place.name }}, {{ place.region }}, {{ place.country }}</button>
+                    <hr>
+                </div>
             </div>
         </div>
     </div>
@@ -50,24 +54,22 @@ export default {
         return {
             formValue: {
                 place: ''
-            }
+            },
+            searchResult: ''
         }
     },
     methods: {
-        searchPlace() {
-            setTimeout(async () => {
-                this.getPlace()
-            }, 3000)
-        },
-    },
-    computed: {
-        async getPlace() {
-            console.log(this.formValue.place)
-            await axios.get(`http://api.weatherapi.com/v1/search.json?key=a0181bd8c8b14fa99aa84630232101&q=${this.formValue.place}`).then((response) => {
-                console.log(response.data)
-            }).catch((err) => {
-                console.log(err)
-            })
+        async searchPlace() {
+            if (this.formValue.place !== '') {
+                console.log(this.formValue.place)
+                await axios.get(`http://api.weatherapi.com/v1/search.json?key=a0181bd8c8b14fa99aa84630232101&q=${this.formValue.place}`).then((response) => {
+                    console.log(response.data)
+                    this.searchResult = response.data
+                    console.log('Result', this.searchResult)
+                }).catch((err) => {
+                    console.log(err)
+                })
+            }
         }
     }
 }
