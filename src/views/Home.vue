dc02bce3b4bf32cb8dc1aec4a55b742b28f53f6f
Milan Sony <milansonyofficial@gmail.com>
2024-04-06 19:02:05 +0530

    working on methods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,19 @@
         <SiteNavigation />
         <!-- Search -->
         <div class="max-w-screen-sm items-center mx-auto p-5">
-            <form class="flex items-center" @submit="submitForm">
+            <form class="flex items-center" @submit.prevent="submitForm">
                 <label for="simple-search" class="sr-only">Search</label>
                 <div class="relative w-full">
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-blue-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
+                    </div>
                     <input type="text" id="simple-search"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 focus:outline-none block w-full ps-10 p-2.5"
-                        placeholder="Search for a city" v-model="formValue.place" required />
+                        placeholder="Search for a city" v-model="formValue.place" @input="searchPlace" required />
                 </div>
                 <button type="submit"
                     class="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300">
@@ -22,6 +29,8 @@
                 </button>
             </form>
             <p class="text-start text-2xl pt-4 font-bold text-white">Search : {{ formValue.place }}</p>
+            <div class="bg-white p-4 rounded-md">
+            </div>
         </div>
     </div>
 </template>
@@ -29,6 +38,8 @@
 <script>
 // components
 import SiteNavigation from '../components/SiteNavigation.vue'
+
+import axios from 'axios'
 
 export default {
     name: 'Home',
@@ -43,9 +54,20 @@ export default {
         }
     },
     methods: {
-        submitForm(event) {
-            event.preventDefault()
-            console.log(this.formValue)
+        searchPlace() {
+            setTimeout(async () => {
+                this.getPlace()
+            }, 3000)
+        },
+    },
+    computed: {
+        async getPlace() {
+            console.log(this.formValue.place)
+            await axios.get(`http://api.weatherapi.com/v1/search.json?key=a0181bd8c8b14fa99aa84630232101&q=${this.formValue.place}`).then((response) => {
+                console.log(response.data)
+            }).catch((err) => {
+                console.log(err)
+            })
         }
     }
 }
